5ad385f94392cd62f386e93d73ed0a1291456f10
유재건 <neversaydie374@gmail.com>
2023-11-20 16:33:40 +0900

    first commit

---
+++ src/main/java/com/team/twodari/TwodariApplication.java
@@ -0,0 +1,13 @@
+package com.team.twodari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwodariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwodariApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team/twodari/TwodariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.twodari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwodariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
