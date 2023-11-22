7ca89247304039903c71e875a88dd460c752c0cf
유재건 <neversaydie374@gmail.com>
2023-11-22 14:16:19 +0900

    etc: 기본 환경 설정 -1차 환경 설정 -컨트롤러,서비스,엔티티 설정 -yml 설정 gradle 설정

--- src/main/java/com/team/twodari/image/entity/ImageEntity.java
+++ src/main/java/com/team/twodari/image/entity/ImageEntity.java
@@ -1,6 +1,6 @@
 package com.team.twodari.image.entity;
 
-import com.team.twodari.common.entity.MutableBaseEntity;
+import com.team.twodari.common.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -14,7 +14,7 @@ import lombok.*;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ImageEntity extends MutableBaseEntity {
+public class ImageEntity extends BaseEntity {
     // 이미지 일련번호
     @Id
     @Column(columnDefinition = "INT")
