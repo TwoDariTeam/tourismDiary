7ca39bbd4b91198d9ae884ae200bdc49f02c7c13
yohan <sjohn9227@naver.com>
2023-11-23 21:53:51 +0900

    refactor: BoardSearchRepositoryImpl 공통 로직 메소드 추출

--- src/main/java/com/team/twodari/board/repository/BoardSearchRepositoryImpl.java
+++ src/main/java/com/team/twodari/board/repository/BoardSearchRepositoryImpl.java
@@ -2,6 +2,8 @@ package com.team.twodari.board.repository;
 
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.team.twodari.board.dto.BoardEntityDto;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +17,7 @@ import java.util.List;
 import static com.team.twodari.board.entity.QBoardEntity.boardEntity;
 import static com.team.twodari.common.constant.Constant.PAGE_SIZE;
 import static com.team.twodari.point.entity.QPointEntity.pointEntity;
+import static com.team.twodari.tag.entity.QTagEntity.*;
 
 @RequiredArgsConstructor
 @Repository
@@ -25,19 +28,8 @@ public class BoardSearchRepositoryImpl implements BoardSearchRepository {
     @Override
     public Slice<BoardEntityDto> findContains(Integer page, String word) {
 
-        List<BoardEntityDto> boardEntities = jpaQueryFactory
-                .select(Projections.constructor(
-                        BoardEntityDto.class,
-                        boardEntity.boardSeq,
-                        boardEntity.categorySeq,
-                        boardEntity.author,
-                        boardEntity.title,
-                        pointEntity.point.sum()
-                ))
-                .from(boardEntity)
-                .leftJoin(pointEntity).on(isEqualBoardSeq())
+        List<BoardEntityDto> boardEntities = createBaseQuery()
                 .where(isContainsWord(word))
-                .groupBy(boardEntity.boardSeq)
                 .orderBy(pointEntity.point.sum().desc())
                 .offset(page * PAGE_SIZE)
                 .limit(PAGE_SIZE + 1)
@@ -49,18 +41,7 @@ public class BoardSearchRepositoryImpl implements BoardSearchRepository {
 
     @Override
     public Slice<BoardEntityDto> findOrderByCreateDate(Integer page) {
-        List<BoardEntityDto> boardEntities = jpaQueryFactory
-                .select(Projections.constructor(
-                        BoardEntityDto.class,
-                        boardEntity.boardSeq,
-                        boardEntity.categorySeq,
-                        boardEntity.author,
-                        boardEntity.title,
-                        pointEntity.point.sum()
-                ))
-                .from(boardEntity)
-                .leftJoin(pointEntity).on(isEqualBoardSeq())
-                .groupBy(boardEntity.boardSeq)
+        List<BoardEntityDto> boardEntities = createBaseQuery()
                 .orderBy(boardEntity.createTime.desc())
                 .offset(page * PAGE_SIZE)
                 .limit(PAGE_SIZE + 1)
@@ -73,7 +54,17 @@ public class BoardSearchRepositoryImpl implements BoardSearchRepository {
 
     @Override
     public Slice<BoardEntityDto> findOrderByPoint(Integer page) {
-        List<BoardEntityDto> boardEntities = jpaQueryFactory
+        List<BoardEntityDto> boardEntities = createBaseQuery()
+                .orderBy(pointEntity.point.sum().desc())
+                .offset(page * PAGE_SIZE)
+                .limit(PAGE_SIZE + 1)
+                .fetch();
+
+        return toSlice(boardEntities);
+    }
+
+    private JPAQuery<BoardEntityDto> createBaseQuery() {
+        return jpaQueryFactory
                 .select(Projections.constructor(
                         BoardEntityDto.class,
                         boardEntity.boardSeq,
@@ -83,22 +74,21 @@ public class BoardSearchRepositoryImpl implements BoardSearchRepository {
                         pointEntity.point.sum()
                 ))
                 .from(boardEntity)
-                .leftJoin(pointEntity).on(isEqualBoardSeq())
-                .groupBy(boardEntity.boardSeq)
-                .orderBy(pointEntity.point.sum().desc())
-                .offset(page * PAGE_SIZE)
-                .limit(PAGE_SIZE + 1)
-                .fetch();
+                .leftJoin(pointEntity).on(isEqualToPointBoardSeq())
+                .leftJoin(tagEntity).on(isEqualToTagBoardSeq())
+                .groupBy(boardEntity.boardSeq);
+    }
 
-        return toSlice(boardEntities);
+    private BooleanExpression isEqualToPointBoardSeq() {
+        return boardEntity.boardSeq.eq(pointEntity.boardSeq);
+    }
+
+    private static BooleanExpression isEqualToTagBoardSeq() {
+        return boardEntity.boardSeq.eq(Expressions.numberTemplate(Long.class, "{0}", tagEntity.boardSeq));
     }
 
     private BooleanExpression isContainsWord(String word) {
         return boardEntity.title.contains(word);
-    }
-
-    private BooleanExpression isEqualBoardSeq() {
-        return boardEntity.boardSeq.eq(pointEntity.boardSeq);
     }
 
     private SliceImpl<BoardEntityDto> toSlice(List<BoardEntityDto> boardEntities) {
