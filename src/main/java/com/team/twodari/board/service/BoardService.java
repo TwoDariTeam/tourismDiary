a4b69ac1881a33b6809c8c648c4d53ee713bf79e
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-11-24 12:56:21 +0900

    feat: BoardService 추가 - 대표 카드 create, update, delete 추가

--- src/main/java/com/team/twodari/board/service/BoardService.java
+++ src/main/java/com/team/twodari/board/service/BoardService.java
@@ -1,9 +1,47 @@
 package com.team.twodari.board.service;
 
+import com.team.twodari.board.dto.BoardCreateDto;
+import com.team.twodari.board.dto.BoardUpdateDto;
+import com.team.twodari.board.entity.BoardEntity;
+import com.team.twodari.board.repository.BoardRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @Transactional
 public class BoardService {
+    private final BoardRepository boardRepository;
+
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    public Long createBoard(BoardCreateDto createDto) {
+        BoardEntity board = BoardEntity.builder()
+                .author(createDto.getAuthor())
+                .title(createDto.getTitle())
+                .accessRole(createDto.getAccessRole())
+                .build();
+        boardRepository.save(board);
+
+        return board.getBoardSeq();
+    }
+
+    public Long updateBoard(Long boardSeq, BoardUpdateDto updateDto) {
+        BoardEntity board = boardRepository.findById(boardSeq).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "게시글이 존재하지 않습니다"));
+        board.updateEntity(updateDto.getTitle(), updateDto.getAccessRole());
+        boardRepository.save(board);
+
+        return board.getBoardSeq();
+    }
+
+    public void deleteBoard(Long boardSeq) {
+        if (!boardRepository.existsById(boardSeq)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "게시글이 존재하지 않습니다");
+        }
+
+        boardRepository.deleteById(boardSeq);
+    }
 }
