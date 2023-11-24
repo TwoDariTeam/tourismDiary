54b65f4bb81e30c60965a0f3f4ff6f9792e00f8b
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-11-24 12:52:14 +0900

    feat: BoardEntity 수정 - GeneratedValue 추가 - updateEntity 추가

--- src/main/java/com/team/twodari/board/entity/BoardEntity.java
+++ src/main/java/com/team/twodari/board/entity/BoardEntity.java
@@ -1,10 +1,7 @@
 package com.team.twodari.board.entity;
 
 import com.team.twodari.common.entity.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Table(name = "TB_BOARD")
@@ -18,6 +15,7 @@ public class BoardEntity extends BaseEntity {
     //시퀀스 네임 찾아서 여기서 주기.
     // 게시판 일련번호
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(columnDefinition = "INT")
     private Long boardSeq;
 
@@ -40,5 +38,8 @@ public class BoardEntity extends BaseEntity {
     @Column(columnDefinition = "CHAR(1)")
     private String deleted;
 
-
+    public void updateEntity(String title, Integer accessRole) {
+        this.title = title;
+        this.accessRole = accessRole;
+    }
 }
