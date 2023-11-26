a69de53967d94b3fa735d8ae5fdd0b24bad0aade
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-11-26 23:29:22 +0900

    feat: ImageRepository 수정

--- src/main/java/com/team/twodari/image/repository/ImageRepository.java
+++ src/main/java/com/team/twodari/image/repository/ImageRepository.java
@@ -1,4 +1,7 @@
 package com.team.twodari.image.repository;
 
-public interface ImageRepository {
+import com.team.twodari.image.entity.ImageEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<ImageEntity, Long> {
 }
