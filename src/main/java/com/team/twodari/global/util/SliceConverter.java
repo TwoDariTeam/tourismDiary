e9e88ad5c1a863221a983ad7de319fe5f04731c0
jooooonj <lee22424205@naver.com>
2023-11-27 14:46:45 +0900

    refactor: 게시글 목록 반환값 생성일자 추가

--- src/main/java/com/team/twodari/global/util/SliceConverter.java
+++ src/main/java/com/team/twodari/global/util/SliceConverter.java
@@ -2,7 +2,6 @@ package com.team.twodari.global.util;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.SliceImpl;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 
