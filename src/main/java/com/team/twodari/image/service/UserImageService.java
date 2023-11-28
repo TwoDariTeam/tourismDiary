ccf7143fe876d188ce88fee50594d4eab1bdc7cd
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-11-28 23:27:08 +0900

    feat: UserImageService, UserImageController 수정 - 유저 프로필 이미지 업로드, 삭제 기능 구현

--- src/main/java/com/team/twodari/image/service/UserImageService.java
+++ src/main/java/com/team/twodari/image/service/UserImageService.java
@@ -1,4 +1,54 @@
 package com.team.twodari.image.service;
 
+import com.team.twodari.common.service.S3UploadService;
+import com.team.twodari.image.entity.UserImageEntity;
+import com.team.twodari.image.repository.UserImageRepository;
+import com.team.twodari.user.entity.UserEntity;
+import com.team.twodari.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class UserImageService {
+    private final UserImageRepository userImageRepository;
+    private final S3UploadService s3UploadService;
+    private final UserRepository userRepository;
+
+    public String uploadUserImage(MultipartFile file, Long userSeq) throws IOException {
+        UserEntity user = userRepository.findById(userSeq)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "사용자가 존재하지 않습니다"));
+
+        String imageUrl = s3UploadService.upload(file);
+
+        UserImageEntity image = UserImageEntity.builder()
+                .user(user)
+                .path(imageUrl)
+                .build();
+
+        userImageRepository.save(image);
+        return imageUrl;
+    }
+
+    public void deleteUserImage(Long userSeq, Long imageSeq) {
+        if (isExistUser(userSeq)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "사용자가 존재하지 않습니다");
+        }
+
+        UserImageEntity image = userImageRepository.findById(imageSeq)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "이미지가 존재하지 않습니다"));
+
+        userImageRepository.delete(image);
+    }
+
+    private boolean isExistUser(Long userSeq) {
+        return userSeq != null && !userRepository.existsById(userSeq);
+    }
 }
