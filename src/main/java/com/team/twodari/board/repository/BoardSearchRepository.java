ad687cd6e68f3674433393fd076f72322c4cab19
yohan <sjohn9227@naver.com>
2023-11-28 12:43:15 +0900

    feat: 컨트롤러 별로 dto를 생성 및 사용, QueryProjection -> Projection으로 변경

--- src/main/java/com/team/twodari/board/repository/BoardSearchRepository.java
+++ src/main/java/com/team/twodari/board/repository/BoardSearchRepository.java
@@ -1,6 +1,8 @@
 package com.team.twodari.board.repository;
 
-import com.team.twodari.board.dto.BoardEntityDto;
+import com.team.twodari.platform.dto.BoardContainsStringDto;
+import com.team.twodari.platform.dto.BoardDateDto;
+import com.team.twodari.platform.dto.BoardPointDto;
 import com.team.twodari.board.entity.BoardLocation;
 
 import java.util.List;
@@ -8,11 +10,13 @@ import java.util.List;
 public interface BoardSearchRepository {
 
 
-    List<BoardEntityDto> findContains(Integer offset,Integer size, BoardLocation location, String word);
+    List<BoardContainsStringDto> findContains(Integer offset,Integer size, BoardLocation location, String word);
 
-    List<BoardEntityDto> findOrderByCreateDate(Long offset,Integer size,BoardLocation location);
+    List<BoardDateDto> findOrderByCreateDate(Long offset,Integer size,BoardLocation location);
 
-    List<BoardEntityDto> findOrderByPoint(Long offset,Integer size,BoardLocation location);
+    List<BoardPointDto> findOrderByPoint(Long offset, Integer size, BoardLocation location);
+
+
 
 
 
--- src/main/java/com/team/twodari/board/repository/BoardSearchRepositoryImpl.java
+++ src/main/java/com/team/twodari/board/repository/BoardSearchRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.team.twodari.board.repository;
 
 import static com.team.twodari.board.entity.BoardLocation.*;
 import static com.team.twodari.board.entity.QBoardEntity.*;
+import static com.team.twodari.image.entity.QBoardImageEntity.*;
 import static com.team.twodari.point.entity.QPointEntity.*;
 import static com.team.twodari.tag.entity.QTagEntity.*;
 
@@ -11,13 +12,14 @@ import java.util.List;
 import org.springframework.stereotype.Repository;
 
 import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Expressions;
-import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.team.twodari.board.dto.BoardEntityDto;
-import com.team.twodari.board.dto.QBoardEntityDto;
 import com.team.twodari.board.entity.BoardLocation;
+import com.team.twodari.platform.dto.BoardContainsStringDto;
+import com.team.twodari.platform.dto.BoardDateDto;
+import com.team.twodari.platform.dto.BoardPointDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -28,9 +30,26 @@ public class BoardSearchRepositoryImpl implements BoardSearchRepository {
 	private final JPAQueryFactory jpaQueryFactory;
 
 	@Override
-	public List<BoardEntityDto> findContains(Integer offset, Integer size, BoardLocation location, String word) {
+	public List<BoardContainsStringDto> findContains(Integer offset, Integer size, BoardLocation location, String word) {
 
-		return createBaseQuery()
+		return jpaQueryFactory
+			.select(
+				Projections.constructor(BoardContainsStringDto.class,
+					boardEntity.boardSeq,
+					boardEntity.categorySeq,
+					boardEntity.author,
+					boardEntity.title,
+					pointEntity.point.sum(),
+					boardEntity.introduce,
+					boardImageEntity.path,
+					boardEntity.createTime,
+					boardEntity.location)
+			)
+			.from(boardEntity)
+			.leftJoin(pointEntity).on(isEqualToPointBoardSeq())
+			.leftJoin(tagEntity).on(isEqualToTagBoardSeq())
+			.leftJoin(boardImageEntity).on(isEqualToImageBoardSeq()).fetchJoin()
+			.groupBy(boardEntity.boardSeq)
 			.where(isContainsWord(word), isEqualLocation(location))
 			.orderBy(orderByTotalPoint(), orderByCreateTime())
 			.offset(offset)
@@ -40,8 +59,26 @@ public class BoardSearchRepositoryImpl implements BoardSearchRepository {
 	}
 
 	@Override
-	public List<BoardEntityDto> findOrderByCreateDate(Long offset, Integer size, BoardLocation location) {
-		return createBaseQuery()
+	public List<BoardDateDto> findOrderByCreateDate(Long offset, Integer size, BoardLocation location) {
+		return jpaQueryFactory
+			.select(
+				Projections.constructor(BoardDateDto.class,
+					boardEntity.boardSeq,
+					boardEntity.categorySeq,
+					boardEntity.author,
+					boardEntity.title,
+					pointEntity.point.sum(),
+					boardEntity.introduce,
+					boardImageEntity.path,
+					boardEntity.createTime,
+					boardEntity.location
+				)
+			)
+			.from(boardEntity)
+			.leftJoin(pointEntity).on(isEqualToPointBoardSeq())
+			.leftJoin(tagEntity).on(isEqualToTagBoardSeq())
+			.leftJoin(boardImageEntity).on(isEqualToImageBoardSeq()).fetchJoin()
+			.groupBy(boardEntity.boardSeq)
 			.where(isEqualLocation(location))
 			.orderBy(orderByCreateTime())
 			.offset(offset)
@@ -50,11 +87,28 @@ public class BoardSearchRepositoryImpl implements BoardSearchRepository {
 	}
 
 	@Override
-	public List<BoardEntityDto> findOrderByPoint(
+	public List<BoardPointDto> findOrderByPoint(
 		Long offset,
 		Integer size,
 		BoardLocation location) {
-		return createBaseQuery()
+		return jpaQueryFactory
+			.select(
+				Projections.constructor(BoardPointDto.class,
+					boardEntity.boardSeq,
+					boardEntity.categorySeq,
+					boardEntity.author,
+					boardEntity.title,
+					pointEntity.point.sum(),
+					boardEntity.introduce,
+					boardImageEntity.path,
+					boardEntity.createTime,
+					boardEntity.location)
+			)
+			.from(boardEntity)
+			.leftJoin(pointEntity).on(isEqualToPointBoardSeq())
+			.leftJoin(tagEntity).on(isEqualToTagBoardSeq())
+			.leftJoin(boardImageEntity).on(isEqualToImageBoardSeq()).fetchJoin()
+			.groupBy(boardEntity.boardSeq)
 			.where(isEqualLocation(location))
 			.orderBy(orderByTotalPoint())
 			.offset(offset)
@@ -63,29 +117,16 @@ public class BoardSearchRepositoryImpl implements BoardSearchRepository {
 
 	}
 
-	private JPAQuery<BoardEntityDto> createBaseQuery() {
-		return jpaQueryFactory
-			.select(
-				new QBoardEntityDto(boardEntity.boardSeq,
-					boardEntity.categorySeq,
-					boardEntity.author,
-					boardEntity.title,
-					pointEntity.point.sum(),
-					boardEntity.createTime,
-					boardEntity.location)
-			)
-			.from(boardEntity)
-			.leftJoin(pointEntity).on(isEqualToPointBoardSeq())
-			.leftJoin(tagEntity).on(isEqualToTagBoardSeq())
-			.groupBy(boardEntity.boardSeq);
-	}
-
 	private BooleanExpression isEqualToPointBoardSeq() {
 		return boardEntity.boardSeq.eq(pointEntity.boardSeq);
 	}
 
 	private BooleanExpression isEqualToTagBoardSeq() {
 		return boardEntity.boardSeq.eq(Expressions.numberTemplate(Long.class, "{0}", tagEntity.boardSeq));
+	}
+
+	private BooleanExpression isEqualToImageBoardSeq() {
+		return boardEntity.boardSeq.eq(boardImageEntity.boardImageSeq);
 	}
 
 	private BooleanExpression isEqualLocation(BoardLocation location) {
