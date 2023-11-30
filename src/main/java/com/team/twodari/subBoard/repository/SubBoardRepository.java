3803fea2b99debfac394e2e4ee51dbd00d9fca4d
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-11-30 22:57:40 +0900

    feat: SubBoardRepository 수정 - 메소드 삭제

--- src/main/java/com/team/twodari/subBoard/repository/SubBoardRepository.java
+++ src/main/java/com/team/twodari/subBoard/repository/SubBoardRepository.java
@@ -3,8 +3,5 @@ package com.team.twodari.subBoard.repository;
 import com.team.twodari.subBoard.entity.SubBoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface SubBoardRepository extends JpaRepository<SubBoardEntity, Long> {
-    List<SubBoardEntity> findByBoardBoardSeq(Long boardSeq);
 }
