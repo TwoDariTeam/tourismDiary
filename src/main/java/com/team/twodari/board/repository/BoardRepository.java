4b0d03a93548fbbd64e4c97baee9956a504ac2cb
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-11-30 23:40:04 +0900

    feat: BoardRepository, SubBoardRepository 수정 - 게시글과 이미지 상세보기 메소드 추가

--- src/main/java/com/team/twodari/board/repository/BoardRepository.java
+++ src/main/java/com/team/twodari/board/repository/BoardRepository.java
@@ -2,6 +2,12 @@ package com.team.twodari.board.repository;
 
 import com.team.twodari.board.entity.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<BoardEntity,Long>,BoardSearchRepository, BoardMyPageRepository {
+    @Query("SELECT b FROM BoardEntity b LEFT JOIN FETCH b.images WHERE b.boardSeq = :boardSeq")
+    Optional<BoardEntity> findByBoardSeqWithImages(@Param("boardSeq") Long boardSeq);
 }
--- src/main/java/com/team/twodari/subBoard/repository/SubBoardRepository.java
+++ src/main/java/com/team/twodari/subBoard/repository/SubBoardRepository.java
@@ -2,6 +2,12 @@ package com.team.twodari.subBoard.repository;
 
 import com.team.twodari.subBoard.entity.SubBoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface SubBoardRepository extends JpaRepository<SubBoardEntity, Long> {
+    @Query("SELECT s FROM SubBoardEntity s LEFT JOIN FETCH s.images WHERE s.subBoardSeq = :subBoardSeq")
+    Optional<SubBoardEntity> findBySubBoardSeqWithImages(@Param("subBoardSeq") Long subBoardSeq);
 }
