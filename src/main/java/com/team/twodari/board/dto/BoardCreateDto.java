8de1ed03878b0541f1c3df41c7f34b67eba82d31
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-11-30 15:55:20 +0900

    feat: BoardCreateDto 수정 - accessRole 필드 삭제

--- src/main/java/com/team/twodari/board/dto/BoardCreateDto.java
+++ src/main/java/com/team/twodari/board/dto/BoardCreateDto.java
@@ -1,7 +1,6 @@
 package com.team.twodari.board.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
@@ -15,7 +14,4 @@ public class BoardCreateDto {
 
     @NotBlank
     private String introduce;
-
-    @NotNull
-    private Integer accessRole;
 }
